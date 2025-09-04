69d5cbf01601a4dbd0c17d375b8e390688ddbfda
¨JesicaM01¨ <jesymerep@gmail.com>
2025-09-04 18:56:25 -0300

    despegue

---
+++ ejercicio9.js
@@ -0,0 +1,13 @@
+// Ejercicio 9: Cuenta regresiva para el despegue
+
+// Uso un "robot" que cuenta de forma descendente.
+// Empiezo en 10.
+// Sigo contando mientras mi número sea mayor o igual a 0.
+// En cada vuelta, le resto 1 a mi número.
+for (let i = 10; i >= 0; i--) {
+  // Muestro el número actual en la consola.
+  console.log(i);
+}
+
+// Al final del ciclo, muestro el mensaje de despegue.
+console.log("¡Despegue!");
